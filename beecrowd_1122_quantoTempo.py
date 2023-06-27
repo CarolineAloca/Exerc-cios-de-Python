380e50823b73aafd1ec819d387e377431ecaaa01
CarolineAloca <81122964+CarolineAloca@users.noreply.github.com>
2023-06-27 20:16:46 -0300

    Add files via upload

---
+++ beecrowd_1122_quantoTempo.py
@@ -0,0 +1,7 @@
+primeiroPercurso = int(input())
+segundoPercurso = int(input())
+terceiroPercurso = int(input())
+
+tempoGasto = (primeiroPercurso + segundoPercurso + terceiroPercurso)
+
+print(tempoGasto, " minutos")
